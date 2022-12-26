514ec1bb6caaf1654920502544f0aeebdf8481d0
evgeniums <evgeniums@dracosha.com>
2022-12-26 18:59:46 +0300

    Add comments.

--- utils/optional_arg.go
+++ utils/optional_arg.go
@@ -1,5 +1,6 @@
 package utils
 
+// Select either default or optional value if the optional value is given in argumments.
 func OptionalArg[T any](defaultArg T, optional ...T) T {
 
 	arg := defaultArg
