8579c14b7b30cb94144f6a8fc29f5c91f3737956
evgeniums <evgeniums@dracosha.com>
2023-01-20 23:10:33 +0300

    Refactor test

--- test/utils_test/test_utils_test.go
+++ test/utils_test/test_utils_test.go
@@ -1,6 +1,7 @@
 package utils_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/evgeniums/go-backend-helpers/pkg/test_utils"
@@ -8,10 +9,10 @@ import (
 
 func TestGlobalTestVars(t *testing.T) {
 
-	t.Logf(`Global testing flags can be set with ldflags when running tests like: go test -ldflags="-X 'github.com/evgeniums/go-backend-helpers/pkg/test_utils.Testing=true'"`)
+	fmt.Println(`Global testing flags can be set with ldflags when running tests like: go test -ldflags="-X 'github.com/evgeniums/go-backend-helpers/pkg/test_utils.Testing=true'"`)
 
-	t.Logf("Current values:")
-	t.Logf("test_utils.Testing=%v", test_utils.Testing)
-	t.Logf("test_utils.ExternalConfigPath=%s", test_utils.ExternalConfigPath)
-	t.Logf("test_utils.Phone=%v", test_utils.Phone)
+	fmt.Printf("Current values:\n")
+	fmt.Printf("test_utils.Testing=%v\n", test_utils.Testing)
+	fmt.Printf("test_utils.ExternalConfigPath=%s\n", test_utils.ExternalConfigPath)
+	fmt.Printf("test_utils.Phone=%v\n", test_utils.Phone)
 }
