3f4302f615efeb43e7d2771d45332b4a2e9d1ab5
evgeniums <evgeniums@dracosha.com>
2023-02-02 14:05:00 +0300

    Merge configuration arrays

--- pkg/config/config_viper/config_viper.go
+++ pkg/config/config_viper/config_viper.go
@@ -1,6 +1,8 @@
 package config_viper
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"path/filepath"
 	"strings"
@@ -21,16 +23,20 @@ func New() *ConfigViper {
 
 func (c *ConfigViper) LoadFile(configFile string, configType ...string) error {
 
+	// setup
 	cfgType := utils.OptionalArg("json", configType...)
 	c.Viper.SetConfigFile(configFile)
 	c.Viper.SetConfigType(cfgType)
 	c.Viper.AddConfigPath(".")
+
+	// read main configuration file
 	err := c.Viper.ReadInConfig()
 	if err != nil {
 		msg := fmt.Errorf("fatal error while reading config file: %s", err)
 		return msg
 	}
 
+	// load includes
 	includes := c.Viper.GetStringSlice("include")
 	for _, include := range includes {
 		if !utils.FileExists(include) {
@@ -50,6 +56,78 @@ func (c *ConfigViper) LoadFile(configFile string, configType ...string) error {
 		}
 	}
 
+	// load includes with arrays for merging
+	if c.Viper.IsSet("include_arrays") {
+
+		// load and merge arrays data
+		includeArrays := c.Viper.Get("include_arrays")
+		includeArraysSlice, ok := includeArrays.([]interface{})
+		if !ok {
+			msg := fmt.Errorf("invalid format of include_arrays section")
+			return msg
+		}
+		for i := range includeArraysSlice {
+			includes := c.Viper.GetStringMapStringSlice(fmt.Sprintf("include_arrays.%d", i))
+			for include, keys := range includes {
+				if !utils.FileExists(include) {
+					// try relative path
+					newInclude := filepath.Join(filepath.Dir(configFile), include)
+					if !utils.FileExists(newInclude) {
+						err = fmt.Errorf("failed to include array config file %s or %s", include, newInclude)
+						return err
+					}
+					include = newInclude
+				}
+				arrCfg := viper.New()
+				arrCfg.SetConfigFile(include)
+				arrCfg.SetConfigType(cfgType)
+				err = arrCfg.ReadInConfig()
+				if err != nil {
+					msg := fmt.Errorf("failed to include array config file %s: %s", include, err)
+					return msg
+				}
+
+				for _, key := range keys {
+					if arrCfg.IsSet(key) {
+						if !c.Viper.IsSet(key) {
+							c.Viper.Set(key, arrCfg.Get(key))
+						} else {
+							arr1 := c.Viper.Get(key)
+							arr2 := arrCfg.Get(key)
+							arr1Slice, ok := arr1.([]interface{})
+							if !ok {
+								msg := fmt.Errorf("failed to include array config file %s: %s is not array in main config", include, key)
+								return msg
+							}
+							arr2Slice, ok := arr2.([]interface{})
+							if !ok {
+								msg := fmt.Errorf("failed to include array config file %s: %s is not array in included config", include, key)
+								return msg
+							}
+							merge := append(arr1Slice, arr2Slice...)
+							c.Viper.Set(key, merge)
+						}
+					}
+				}
+			}
+		}
+
+		// reload viper configuration
+		all := c.Viper.AllSettings()
+		b, err := json.MarshalIndent(all, "", " ")
+		if err != nil {
+			msg := fmt.Errorf("failed to marshal viper settings: %s", err)
+			return msg
+		}
+		c.Viper = viper.New()
+		c.Viper.SetConfigType(cfgType)
+		if err = c.Viper.ReadConfig(bytes.NewReader(b)); err != nil {
+			msg := fmt.Errorf("failed to re-read viper settings: %s", err)
+			return msg
+		}
+	}
+
+	// done
 	return nil
 }
 
--- test/config_test/json_config_test.go
+++ test/config_test/json_config_test.go
@@ -20,11 +20,28 @@ func TestLoadJsonConfig(t *testing.T) {
 	require.NoErrorf(t, app.Init(configFile), "failed to init application context")
 	defer app.Close()
 
+	assert.True(t, app.Cfg().IsSet("include"))
+	assert.True(t, app.Cfg().IsSet("include_arrays"))
+
 	assert.True(t, app.Cfg().IsSet("main_section.parameter1"))
+	assert.True(t, app.Cfg().IsSet("main_section.array_parameter.0"))
 	assert.True(t, app.Cfg().IsSet("additional_section.parameter3"))
 	assert.True(t, app.Cfg().IsSet("main_section.additional_parameter2"))
+	assert.True(t, app.Cfg().IsSet("main_section.override_parameter"))
+	assert.True(t, app.Cfg().IsSet("main_section.map_parameter.nested_parameter1"))
+	assert.True(t, app.Cfg().IsSet("main_section.map_parameter.nested_parameter2"))
+	assert.True(t, app.Cfg().IsSet("main_section.array_parameter.1"))
+
+	assert.Equal(t, "item1", app.Cfg().GetString("main_section.array_parameter.0"))
+	assert.Equal(t, "item2", app.Cfg().GetString("main_section.array_parameter.1"))
+
+	arrayParameter := app.Cfg().GetStringSlice("main_section.array_parameter")
+	require.Equal(t, 2, len(arrayParameter))
+	assert.Equal(t, "item1", arrayParameter[0])
+	assert.Equal(t, "item2", arrayParameter[1])
 
 	assert.Equal(t, "value1", app.Cfg().GetString("main_section.parameter1"))
 	assert.Equal(t, "value2", app.Cfg().GetString("main_section.additional_parameter2"))
 	assert.Equal(t, "value3", app.Cfg().GetString("additional_section.parameter3"))
+	assert.Equal(t, "new_value", app.Cfg().GetString("main_section.override_parameter"))
 }
