8de8c439a578f6f3e5f8c84c47f2ee569a27a507
evgeniums <evgeniums@dracosha.com>
2023-02-03 20:48:33 +0300

    Set configuration parameters from command arguments

---
+++ pkg/config/config_args.go
@@ -0,0 +1,130 @@
+package config
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/evgeniums/go-backend-helpers/pkg/utils"
+)
+
+const (
+	TypeEmpty      = ""
+	TypeBool       = "bool"
+	TypeInt        = "int"
+	TypeIntList    = "int_list"
+	TypeFloat      = "float"
+	TypeFloatList  = "float_list"
+	TypeString     = "string"
+	TypeStringList = "string_list"
+)
+
+var knownArgTypes = map[string]bool{
+	TypeEmpty:      true,
+	TypeBool:       true,
+	TypeInt:        true,
+	TypeFloat:      true,
+	TypeString:     true,
+	TypeIntList:    true,
+	TypeFloatList:  true,
+	TypeStringList: true,
+}
+
+func setParameter(cfg Config, valueType string, key string, value string) error {
+
+	switch valueType {
+	case TypeEmpty:
+		cfg.Set(key, value)
+	case TypeString:
+		cfg.Set(key, value)
+	case TypeBool:
+		val := strings.ToLower(value)
+		cfg.Set(key, val == "true")
+	case TypeInt:
+		val, err := strconv.Atoi(value)
+		if err != nil {
+			return fmt.Errorf("parameter %s must be integer", key)
+		}
+		cfg.Set(key, val)
+	case TypeFloat:
+		val, err := utils.StrToFloat(value)
+		if err != nil {
+			return fmt.Errorf("parameter %s must be float", key)
+		}
+		cfg.Set(key, val)
+	case TypeStringList:
+		vals := strings.Split(value, ",")
+		cfg.Set(key, vals)
+	case TypeIntList:
+		{
+			strVals := strings.Split(value, ",")
+			vals := make([]int, len(strVals))
+			for i, strVal := range strVals {
+				val, err := strconv.Atoi(strVal)
+				if err != nil {
+					return fmt.Errorf("item %d of parameter %s must be integer", i, key)
+				}
+				vals[i] = val
+			}
+			cfg.Set(key, vals)
+		}
+	case TypeFloatList:
+		{
+			strVals := strings.Split(value, ",")
+			vals := make([]float64, len(strVals))
+			for i, strVal := range strVals {
+				val, err := utils.StrToFloat(strVal)
+				if err != nil {
+					return fmt.Errorf("item %d of parameter %s must be float", i, key)
+				}
+				vals[i] = val
+			}
+			cfg.Set(key, vals)
+		}
+	}
+
+	return nil
+}
+
+func LoadArgs(cfg Config, args []string) error {
+
+	if len(args)%2 != 0 {
+		return fmt.Errorf("invalid number of configuration parameters in arguments: %d (mus be even)", len(args))
+	}
+
+	i := 0
+	for i < len(args) {
+
+		key := args[i]
+		i++
+		value := args[i]
+		i++
+
+		if !strings.HasPrefix(key, "--") {
+			return fmt.Errorf("name of configuration parameter (%s) must start with --", key)
+		}
+
+		key = key[2:]
+		key = strings.ToLower(key)
+		keyParts := strings.Split(key, ".")
+		if len(keyParts) < 2 {
+			return fmt.Errorf("last section of configuration parameter name (%s) must denote the parameter's type", key)
+		}
+
+		parameterType := keyParts[len(keyParts)-1]
+		_, ok := knownArgTypes[parameterType]
+		if !ok {
+			return fmt.Errorf("unknown type %s of configuration parameter %s", parameterType, key)
+		}
+
+		keyParts = keyParts[:len(keyParts)-1]
+		key = strings.Join(keyParts, ".")
+
+		err := setParameter(cfg, parameterType, key, value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
---
+++ test/config_test/config_args_test.go
@@ -0,0 +1,48 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/evgeniums/go-backend-helpers/pkg/app_context/app_default"
+	"github.com/evgeniums/go-backend-helpers/pkg/config"
+	"github.com/evgeniums/go-backend-helpers/pkg/test_utils"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestConfigArgs(t *testing.T) {
+	configFile := test_utils.AssetsFilePath(testDir, "main.jsonc")
+	app := app_default.New(nil)
+	require.NoErrorf(t, app.Init(configFile), "failed to init application context")
+	defer app.Close()
+
+	assert.Equal(t, "value1", app.Cfg().GetString("main_section.parameter1"))
+	assert.InDeltaSlice(t, []float64{100.99, 200.02, 300.03}, app.Cfg().GetIntSlice("main_section.subsection.float_list"), 0.001)
+
+	args := []string{
+		"--main_section.parameter1.string", "arg_value1",
+		"--main_section.parameter2.", "arg_value2",
+		"--main_section.subsection.int_parameter.int", "1234",
+		"--main_section.subsection.float_parameter.float", "100,29",
+		"--main_section.subsection.bool_parameter.bool", "true",
+		"--main_section.subsection.list.string_list", "one,two,three",
+		"--main_section.subsection.ilist.int_list", "1,2,3",
+		"--main_section.subsection.flist.float_list", "100.99,200.02,300.03",
+	}
+	require.NoErrorf(t, config.LoadArgs(app.Cfg(), args), "failed to load args")
+
+	assert.Equal(t, "arg_value1", app.Cfg().GetString("main_section.parameter1"))
+	assert.Equal(t, "arg_value2", app.Cfg().GetString("main_section.parameter2"))
+	assert.Equal(t, 1234, app.Cfg().GetInt("main_section.subsection.int_parameter"))
+	assert.InDelta(t, 100.29, app.Cfg().GetFloat64("main_section.subsection.float_parameter"), 0.001)
+	assert.True(t, app.Cfg().GetBool("main_section.subsection.bool_parameter"))
+	assert.Equal(t, []string{"one", "two", "three"}, app.Cfg().GetStringSlice("main_section.subsection.list"))
+	assert.Equal(t, []int{1, 2, 3}, app.Cfg().GetIntSlice("main_section.subsection.ilist"))
+	flist := app.Cfg().Get("main_section.subsection.flist")
+	flistSlice, ok := flist.([]float64)
+	require.True(t, ok)
+	require.Equal(t, 3, len(flistSlice))
+	assert.InDelta(t, 100.99, flistSlice[0], 0.001)
+	assert.InDelta(t, 200.02, flistSlice[1], 0.001)
+	assert.InDelta(t, 300.03, flistSlice[2], 0.001)
+}
