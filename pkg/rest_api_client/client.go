8ce1b9f1b9cded55e73b6ef1e89cbf1efa50b78c
evgeniums <evgeniums@dracosha.com>
2023-02-04 20:24:06 +0300

    Add RestApiClient interface

--- pkg/rest_api_client/client.go
+++ pkg/rest_api_client/client.go
@@ -15,23 +15,28 @@ import (
 	"github.com/google/go-querystring/query"
 )
 
-type DoRequest = func(ctx op_context.Context, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error)
+type RestApiClient interface {
+	Url(path string) string
 
-func fillResponseError(resp Response) error {
-	b := resp.Body()
-	if b != nil {
-		errResp := &rest_api_gin_server.ResponseError{}
-		err := json.Unmarshal(b, errResp)
-		if err != nil {
-			return err
-		}
-		resp.SetError(errResp)
-		return nil
-	}
-	return nil
+	Login(ctx op_context.Context, user string, password string) (Response, error)
+	Logout(ctx op_context.Context) (Response, error)
+
+	UpdateTokens(ctx op_context.Context)
+	UpdateCsrfToken(ctx op_context.Context) (Response, error)
+	RequestRefreshToken(ctx op_context.Context) (Response, error)
+
+	Post(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string)
+	Put(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error)
+	Patch(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error)
+	Get(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error)
+	Delete(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error)
+
+	SendSmsConfirmation(send DoRequest, ctx op_context.Context, resp Response, code string, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error)
 }
 
-type HttpClient struct {
+type DoRequest = func(ctx op_context.Context, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error)
+
+type RestApiClientBase struct {
 	BaseUrl      string
 	AccessToken  string
 	RefreshToken string
@@ -41,22 +46,22 @@ type HttpClient struct {
 	SendWithQuery DoRequest
 }
 
-func NewHttpClient(baseUrl string, withBodySender DoRequest, withQuerySender DoRequest) *HttpClient {
-	c := &HttpClient{}
+func NewRestApiClientBase(baseUrl string, withBodySender DoRequest, withQuerySender DoRequest) *RestApiClientBase {
+	c := &RestApiClientBase{}
 	c.BaseUrl = baseUrl
 	c.SendWithBody = withBodySender
 	c.SendWithQuery = withQuerySender
 	return c
 }
 
-func (c *HttpClient) Url(path string) string {
+func (c *RestApiClientBase) Url(path string) string {
 	return c.BaseUrl + path
 }
 
-func (h *HttpClient) Login(ctx op_context.Context, user string, password string) (Response, error) {
+func (h *RestApiClientBase) Login(ctx op_context.Context, user string, password string) (Response, error) {
 
 	var err error
-	c := ctx.TraceInMethod("HttpClient.Login", logger.Fields{"user": user})
+	c := ctx.TraceInMethod("RestApiClientBase.Login", logger.Fields{"user": user})
 	onExit := func() {
 		if err != nil {
 			c.SetError(err)
@@ -69,7 +74,7 @@ func (h *HttpClient) Login(ctx op_context.Context, user string, password string)
 
 	// first step
 	headers := map[string]string{"x-auth-login": user}
-	resp, err := h.Post(ctx, path, nil, headers)
+	resp, err := h.Post(ctx, path, nil, nil, headers)
 	if err != nil {
 		c.SetMessage("failed to send first request")
 		return nil, err
@@ -84,7 +89,7 @@ func (h *HttpClient) Login(ctx op_context.Context, user string, password string)
 	salt := resp.Header().Get("x-auth-login-salt")
 	phash := auth_login_phash.Phash(password, salt)
 	headers["x-auth-login-phash"] = phash
-	resp, err = h.Post(ctx, path, nil, headers)
+	resp, err = h.Post(ctx, path, nil, nil, headers)
 	if err != nil {
 		c.SetMessage("failed to send second request")
 		return nil, err
@@ -99,7 +104,7 @@ func (h *HttpClient) Login(ctx op_context.Context, user string, password string)
 	return resp, nil
 }
 
-func (c *HttpClient) addTokens(headers ...map[string]string) map[string]string {
+func (c *RestApiClientBase) addTokens(headers ...map[string]string) map[string]string {
 
 	h := map[string]string{}
 	if c.AccessToken != "" {
@@ -114,7 +119,7 @@ func (c *HttpClient) addTokens(headers ...map[string]string) map[string]string {
 	return h
 }
 
-func (c *HttpClient) updateTokens(resp Response) {
+func (c *RestApiClientBase) updateTokens(resp Response) {
 	accessToken := resp.Header().Get("x-auth-access-token")
 	if accessToken != "" {
 		c.AccessToken = accessToken
@@ -129,9 +134,9 @@ func (c *HttpClient) updateTokens(resp Response) {
 	}
 }
 
-func (h *HttpClient) SendSmsConfirmation(send DoRequest, ctx op_context.Context, resp Response, code string, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
+func (h *RestApiClientBase) SendSmsConfirmation(send DoRequest, ctx op_context.Context, resp Response, code string, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
 
-	c := ctx.TraceInMethod("HttpClient.SendSmsConfirmation")
+	c := ctx.TraceInMethod("RestApiClientBase.SendSmsConfirmation")
 	defer ctx.TraceOutMethod()
 
 	hs := h.addTokens(headers...)
@@ -140,20 +145,24 @@ func (h *HttpClient) SendSmsConfirmation(send DoRequest, ctx op_context.Context,
 	if token != "" {
 		hs["x-auth-sms-token"] = token
 	}
-	nextResp, err := h.sendRequest(send, ctx, method, path, cmd, hs)
+	nextResp, err := h.sendRequest(send, ctx, method, path, cmd, nil, hs)
 	if err != nil {
 		return nil, c.SetError(err)
 	}
 	return nextResp, nil
 }
 
-func (h *HttpClient) sendRequest(send DoRequest, ctx op_context.Context, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
+func (h *RestApiClientBase) sendRequest(send DoRequest, ctx op_context.Context, method string, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
 
-	c := ctx.TraceInMethod("HttpClient.sendRequest", logger.Fields{"method": method, "path": path})
+	// setup
+	c := ctx.TraceInMethod("RestApiClientBase.sendRequest", logger.Fields{"method": method, "path": path})
 	defer ctx.TraceOutMethod()
 
+	// prepare tokens
 	hs := h.addTokens(headers...)
 	h.addTokens(headers...)
+
+	// send request
 	resp, err := send(ctx, method, h.Url(path), cmd, hs)
 	if err != nil {
 		c.SetMessage("failed to send request")
@@ -161,57 +170,69 @@ func (h *HttpClient) sendRequest(send DoRequest, ctx op_context.Context, method 
 	}
 	h.updateTokens(resp)
 
+	// fill response error
 	err = fillResponseError(resp)
 	if err != nil {
 		c.SetMessage("failed to parse response error")
-		return nil, c.SetError(err)
+		return resp, c.SetError(err)
 	}
 
+	// fill good response
+	if response != nil && resp.Code() < http.StatusBadRequest {
+		if resp.Code() < http.StatusBadRequest {
+			err = json.Unmarshal(resp.Body(), response)
+			if err != nil {
+				c.SetMessage("failed to parse response message")
+			}
+		}
+	}
+
+	// done
 	return resp, nil
 }
 
-func (h *HttpClient) RequestBody(ctx op_context.Context, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.sendRequest(h.SendWithBody, ctx, method, path, cmd, headers...)
+func (h *RestApiClientBase) RequestBody(ctx op_context.Context, method string, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.sendRequest(h.SendWithBody, ctx, method, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) RequestQuery(ctx op_context.Context, method string, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.sendRequest(h.SendWithQuery, ctx, method, path, cmd, headers...)
+func (h *RestApiClientBase) RequestQuery(ctx op_context.Context, method string, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.sendRequest(h.SendWithQuery, ctx, method, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Post(ctx op_context.Context, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.RequestBody(ctx, http.MethodPost, path, cmd, headers...)
+func (h *RestApiClientBase) Post(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.RequestBody(ctx, http.MethodPost, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Put(ctx op_context.Context, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.RequestBody(ctx, http.MethodPut, path, cmd, headers...)
+func (h *RestApiClientBase) Put(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.RequestBody(ctx, http.MethodPut, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Patch(ctx op_context.Context, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.RequestBody(ctx, http.MethodPatch, path, cmd, headers...)
+func (h *RestApiClientBase) Patch(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.RequestBody(ctx, http.MethodPatch, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Get(ctx op_context.Context, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.RequestQuery(ctx, http.MethodGet, path, cmd, headers...)
+func (h *RestApiClientBase) Get(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.RequestQuery(ctx, http.MethodGet, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Delete(ctx op_context.Context, path string, cmd interface{}, headers ...map[string]string) (Response, error) {
-	return h.RequestQuery(ctx, http.MethodGet, path, cmd, headers...)
+func (h *RestApiClientBase) Delete(ctx op_context.Context, path string, cmd interface{}, response interface{}, headers ...map[string]string) (Response, error) {
+	return h.RequestQuery(ctx, http.MethodGet, path, cmd, response, headers...)
 }
 
-func (h *HttpClient) Logout(ctx op_context.Context) (Response, error) {
-	c := ctx.TraceInMethod("HttpClient.Logout")
+func (h *RestApiClientBase) Logout(ctx op_context.Context) (Response, error) {
+	c := ctx.TraceInMethod("RestApiClientBase.Logout")
 	defer ctx.TraceOutMethod()
-	resp, err := h.Post(ctx, "/auth/logout", nil)
+	resp, err := h.Post(ctx, "/auth/logout", nil, nil)
 	if err != nil {
 		return nil, c.SetError(err)
 	}
 	return resp, nil
 }
 
-func (h *HttpClient) UpdateCsrfToken(ctx op_context.Context) (Response, error) {
-	c := ctx.TraceInMethod("HttpClient.UpdateCsrfToken")
+func (h *RestApiClientBase) UpdateCsrfToken(ctx op_context.Context) (Response, error) {
+	c := ctx.TraceInMethod("RestApiClientBase.UpdateCsrfToken")
 	defer ctx.TraceOutMethod()
-	resp, err := h.Get(ctx, "/status/check", nil)
+	resp, err := h.Get(ctx, "/status/check", nil, nil)
 	if err != nil {
 		return nil, c.SetError(err)
 	}
@@ -223,9 +244,9 @@ func (h *HttpClient) UpdateCsrfToken(ctx op_context.Context) (Response, error) {
 	return resp, nil
 }
 
-func (h *HttpClient) UpdateTokens(ctx op_context.Context) (Response, error) {
+func (h *RestApiClientBase) UpdateTokens(ctx op_context.Context) (Response, error) {
 
-	c := ctx.TraceInMethod("HttpClient.UpdateTokens")
+	c := ctx.TraceInMethod("RestApiClientBase.UpdateTokens")
 	defer ctx.TraceOutMethod()
 
 	resp, err := h.UpdateCsrfToken(ctx)
@@ -241,13 +262,13 @@ func (h *HttpClient) UpdateTokens(ctx op_context.Context) (Response, error) {
 	return resp, nil
 }
 
-func (h *HttpClient) RequestRefreshToken(ctx op_context.Context) (Response, error) {
+func (h *RestApiClientBase) RequestRefreshToken(ctx op_context.Context) (Response, error) {
 
-	c := ctx.TraceInMethod("HttpClient.RequestRefreshToken")
+	c := ctx.TraceInMethod("RestApiClientBase.RequestRefreshToken")
 	defer ctx.TraceOutMethod()
 
 	headers := map[string]string{"x-auth-refresh-token": h.RefreshToken}
-	resp, err := h.Post(ctx, "/auth/refresh", nil, headers)
+	resp, err := h.Post(ctx, "/auth/refresh", nil, nil, headers)
 	if err != nil {
 		return nil, c.SetError(err)
 	}
@@ -259,7 +280,7 @@ func (h *HttpClient) RequestRefreshToken(ctx op_context.Context) (Response, erro
 	return resp, nil
 }
 
-func (h *HttpClient) Prepare(ctx op_context.Context) (Response, error) {
+func (h *RestApiClientBase) Prepare(ctx op_context.Context) (Response, error) {
 	return h.UpdateCsrfToken(ctx)
 }
 
@@ -345,6 +366,20 @@ func DefaultSendWithQuery(ctx op_context.Context, method string, url string, cmd
 	return resp, nil
 }
 
-func DefaultHttpClient(baseUrl string) *HttpClient {
-	return NewHttpClient(baseUrl, DefaultSendWithBody, DefaultSendWithQuery)
+func DefaultRestApiClientBase(baseUrl string) *RestApiClientBase {
+	return NewRestApiClientBase(baseUrl, DefaultSendWithBody, DefaultSendWithQuery)
+}
+
+func fillResponseError(resp Response) error {
+	b := resp.Body()
+	if b != nil {
+		errResp := &rest_api_gin_server.ResponseError{}
+		err := json.Unmarshal(b, errResp)
+		if err != nil {
+			return err
+		}
+		resp.SetError(errResp)
+		return nil
+	}
+	return nil
 }
