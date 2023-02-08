d42c22be5dafd7b4c01f2d26cdc47578e5f185ec
evgeniums <evgeniums@dracosha.com>
2023-02-08 09:33:55 +0300

    Add admin roles

--- pkg/admin/admin.go
+++ pkg/admin/admin.go
@@ -1,16 +1,27 @@
 package admin
 
 import (
+	"github.com/evgeniums/go-backend-helpers/pkg/common"
 	"github.com/evgeniums/go-backend-helpers/pkg/user/user_session_default"
 	"github.com/evgeniums/go-backend-helpers/pkg/user_manager"
 )
 
+type Role struct {
+	common.IDBase
+	Name string
+}
+
 type Admin struct {
 	user_session_default.User
+	Roles []Role `gorm:"-:all"`
 }
 
 func NewAdmin() *Admin {
-	return &Admin{}
+	// TODO fill roles from database
+	a := &Admin{}
+	a.Roles = make([]Role, 1)
+	a.Roles[0] = Role{Name: "superadmin"}
+	return a
 }
 
 type AdminSession struct {
