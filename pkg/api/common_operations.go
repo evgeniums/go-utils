d524b9cc553479c09479a8d2bcf81888ee602416
evgeniums <evgeniums@dracosha.com>
2023-02-19 14:04:01 +0300

    Fix operations naming

--- pkg/api/common_operations.go
+++ pkg/api/common_operations.go
@@ -12,27 +12,31 @@ func NamedResourceOperation(sampleResource Resource, resourceName string, resour
 }
 
 func Add(name string) Operation {
-	return NewOperation("add", access_control.Create)
+	return NewOperation(name, access_control.Create)
 }
 
 func Find(name string) Operation {
-	return NewOperation("find", access_control.Get)
+	return NewOperation(name, access_control.Get)
+}
+
+func Exists(name string) Operation {
+	return NewOperation(name, access_control.Get)
 }
 
 func List(name string) Operation {
-	return NewOperation("list", access_control.Read)
+	return NewOperation(name, access_control.Read)
 }
 
 func Delete(name string) Operation {
-	return NewOperation("delete", access_control.Delete)
+	return NewOperation(name, access_control.Delete)
 }
 
 func Update(name string) Operation {
-	return NewOperation("update", access_control.Put)
+	return NewOperation(name, access_control.Put)
 }
 
 func UpdatePartial(name string) Operation {
-	return NewOperation("update_partial", access_control.Patch)
+	return NewOperation(name, access_control.Patch)
 }
 
 type UpdateCmd struct {
@@ -40,9 +44,9 @@ type UpdateCmd struct {
 }
 
 func Bind(name string) Operation {
-	return NewOperation("bind", access_control.Create)
+	return NewOperation(name, access_control.Create)
 }
 
 func Unbind(name string) Operation {
-	return NewOperation("unbind", access_control.Delete)
+	return NewOperation(name, access_control.Delete)
 }
