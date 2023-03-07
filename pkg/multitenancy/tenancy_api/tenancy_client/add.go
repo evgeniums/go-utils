f674ea3ae1cdeec95083956faf4b2b83167dbd53
evgeniums <evgeniums@dracosha.com>
2023-03-07 15:23:28 +0300

    Add generic handler for APi client

--- pkg/multitenancy/tenancy_api/tenancy_client/add.go
+++ pkg/multitenancy/tenancy_api/tenancy_client/add.go
@@ -1,27 +1,11 @@
 package tenancy_client
 
 import (
-	"github.com/evgeniums/go-backend-helpers/pkg/api"
 	"github.com/evgeniums/go-backend-helpers/pkg/api/api_client"
 	"github.com/evgeniums/go-backend-helpers/pkg/multitenancy"
 	"github.com/evgeniums/go-backend-helpers/pkg/multitenancy/tenancy_api"
 	"github.com/evgeniums/go-backend-helpers/pkg/op_context"
 )
-
-type Add struct {
-	cmd    *multitenancy.TenancyData
-	result *tenancy_api.TenancyResponse
-}
-
-func (a *Add) Exec(client api_client.Client, ctx op_context.Context, operation api.Operation) error {
-
-	c := ctx.TraceInMethod("AddTenancy.Exec")
-	defer ctx.TraceOutMethod()
-
-	err := client.Exec(ctx, operation, a.cmd, a.result)
-	c.SetError(err)
-	return err
-}
 
 func (t *TenancyClient) Add(ctx op_context.Context, tenancy *multitenancy.TenancyData) (*multitenancy.TenancyItem, error) {
 
@@ -37,10 +21,7 @@ func (t *TenancyClient) Add(ctx op_context.Context, tenancy *multitenancy.Tenanc
 	defer onExit()
 
 	// prepare and exec handler
-	handler := &Add{
-		cmd:    tenancy,
-		result: &tenancy_api.TenancyResponse{},
-	}
+	handler := api_client.NewHandler(tenancy, &tenancy_api.TenancyResponse{})
 	err = t.add.Exec(ctx, api_client.MakeOperationHandler(t.Client(), handler))
 	if err != nil {
 		c.SetMessage("failed to exec operation")
@@ -48,5 +29,5 @@ func (t *TenancyClient) Add(ctx op_context.Context, tenancy *multitenancy.Tenanc
 	}
 
 	// done
-	return handler.result.TenancyItem, nil
+	return handler.Result.TenancyItem, nil
 }
