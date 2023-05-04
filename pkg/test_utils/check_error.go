0cd76c3e6afe7742d0cbdaabd774f86eb711d25a
evgeniums <evgeniums@dracosha.com>
2023-05-04 18:07:58 +0300

    Show context errors in tests

--- pkg/test_utils/check_error.go
+++ pkg/test_utils/check_error.go
@@ -3,8 +3,10 @@ package test_utils
 import (
 	"testing"
 
+	"github.com/evgeniums/go-backend-helpers/pkg/app_context"
 	"github.com/evgeniums/go-backend-helpers/pkg/common"
 	"github.com/evgeniums/go-backend-helpers/pkg/generic_error"
+	"github.com/evgeniums/go-backend-helpers/pkg/op_context"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -27,4 +29,18 @@ func ObjectEqual(t *testing.T, left common.Object, right common.Object) {
 		right.SetUpDatedAt(left.GetCreatedAt())
 	}
 	assert.Equal(t, left, right)
+}
+
+func NoError(t *testing.T, ctx op_context.Context, err error) {
+	if err != nil && ctx != nil {
+		ctx.Close()
+	}
+	require.NoError(t, err)
+}
+
+func NoErrorApp(t *testing.T, app app_context.Context, err error) {
+	if err != nil && app != nil {
+		app.Logger().CheckFatalStack(app.Logger())
+	}
+	require.NoError(t, err)
 }
