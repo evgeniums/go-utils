370dd1ca2cb62ee3428418e6e3b811f42b0af13b
evgeniums <evgeniums@dracosha.com>
2023-05-06 15:30:10 +0300

    Configure bare bone server from pool service

--- pkg/api/bare_bones_server/bare_bones_server.go
+++ pkg/api/bare_bones_server/bare_bones_server.go
@@ -9,7 +9,9 @@ import (
 	"github.com/evgeniums/go-backend-helpers/pkg/auth/auth_service"
 	"github.com/evgeniums/go-backend-helpers/pkg/auth/auth_session"
 	"github.com/evgeniums/go-backend-helpers/pkg/config/object_config"
+	"github.com/evgeniums/go-backend-helpers/pkg/logger"
 	"github.com/evgeniums/go-backend-helpers/pkg/multitenancy"
+	"github.com/evgeniums/go-backend-helpers/pkg/pool/app_with_pools"
 	"github.com/evgeniums/go-backend-helpers/pkg/signature"
 	"github.com/evgeniums/go-backend-helpers/pkg/sms"
 	"github.com/evgeniums/go-backend-helpers/pkg/utils"
@@ -33,6 +35,10 @@ type Config struct {
 	WithoutDynamicTables bool
 
 	MultitenancyBaseServices bool
+
+	DefaultPoolServiceName    string
+	DefaultPoolServiceType    string
+	DefaultPrivatePoolService bool
 }
 
 type pimpl struct {
@@ -44,13 +50,25 @@ type pimpl struct {
 	signatureManager signature.SignatureManager
 }
 
+type BareBonesServerBaseConfig struct {
+	POOL_SERVICE_NAME    string
+	POOL_SERVICE_TYPE    string
+	PRIVATE_POOL_SERVICE bool
+}
+
 type BareBonesServerBase struct {
 	pimpl
+
+	config BareBonesServerBaseConfig
 
 	WithoutStatusService bool
 	WithoutDynamicTables bool
 
 	MultitenancyBaseServices bool
+}
+
+func (s *BareBonesServerBase) Config() interface{} {
+	return &s.config
 }
 
 func (s *BareBonesServerBase) Construct(users auth_session.WithUserSessionManager, config ...Config) {
@@ -66,6 +84,10 @@ func (s *BareBonesServerBase) Construct(users auth_session.WithUserSessionManage
 		s.WithoutDynamicTables = cfg.WithoutDynamicTables
 		s.WithoutStatusService = cfg.WithoutStatusService
 		s.MultitenancyBaseServices = cfg.MultitenancyBaseServices
+
+		s.config.POOL_SERVICE_NAME = cfg.DefaultPoolServiceName
+		s.config.POOL_SERVICE_TYPE = cfg.DefaultPoolServiceType
+		s.config.PRIVATE_POOL_SERVICE = cfg.DefaultPrivatePoolService
 	}
 }
 
@@ -78,6 +100,12 @@ func New(users auth_session.WithUserSessionManager, config ...Config) *BareBones
 func (s *BareBonesServerBase) Init(app app_context.Context, tenancyManager multitenancy.Multitenancy, configPath ...string) error {
 
 	path := utils.OptionalArg("server", configPath...)
+
+	// load configuration
+	err := object_config.LoadLogValidate(app.Cfg(), app.Logger(), app.Validator(), s, path)
+	if err != nil {
+		return app.Logger().PushFatalStack("failed to load bare bone server configuration", err)
+	}
 
 	// init SMS manager
 	if s.pimpl.smsManager == nil && s.pimpl.smsProviders != nil {
@@ -93,7 +121,7 @@ func (s *BareBonesServerBase) Init(app app_context.Context, tenancyManager multi
 	if s.pimpl.auth == nil {
 		auth := auth.NewAuth()
 		authPath := object_config.Key(path, "auth")
-		err := auth.Init(app.Cfg(), app.Logger(), app.Validator(), &auth_factory.DefaultAuthFactory{Users: s.pimpl.users, SmsManager: s.pimpl.smsManager, SignatureManager: s.pimpl.signatureManager}, authPath)
+		err = auth.Init(app.Cfg(), app.Logger(), app.Validator(), &auth_factory.DefaultAuthFactory{Users: s.pimpl.users, SmsManager: s.pimpl.smsManager, SignatureManager: s.pimpl.signatureManager}, authPath)
 		if err != nil {
 			return app.Logger().PushFatalStack("failed to init auth manager", err)
 		}
@@ -102,9 +130,15 @@ func (s *BareBonesServerBase) Init(app app_context.Context, tenancyManager multi
 
 	// init REST API server
 	if s.pimpl.server == nil {
+
 		server := rest_api_gin_server.NewServer()
+		err = s.initFromPoolService(app, server)
+		if err != nil {
+			return err
+		}
+
 		serverPath := object_config.Key(path, "rest_api_server")
-		err := server.Init(app, s.pimpl.auth, tenancyManager, serverPath)
+		err = server.Init(app, s.pimpl.auth, tenancyManager, serverPath)
 		if err != nil {
 			return app.Logger().PushFatalStack("failed to init REST API server", err)
 		}
@@ -121,6 +155,42 @@ func (s *BareBonesServerBase) Init(app app_context.Context, tenancyManager multi
 	api_server.AddServiceToServer(s.pimpl.server, auth_service.NewAuthService(s.MultitenancyBaseServices))
 
 	// done
+	return nil
+}
+
+func (s *BareBonesServerBase) initFromPoolService(app app_context.Context, restApiServer *rest_api_gin_server.Server) error {
+
+	if s.config.POOL_SERVICE_NAME != "" {
+
+		poolApp, ok := app.(app_with_pools.AppWithPools)
+		if !ok {
+			return app.Logger().PushFatalStack("invalid application type, must be pool app", nil)
+		}
+
+		// check if app with self pool
+		selfPool, err := poolApp.Pools().SelfPool()
+		if err != nil {
+			return app.Logger().PushFatalStack("self pool must be specified for API server", err)
+		}
+
+		// find service for role
+		service, err := selfPool.ServiceByName(s.config.POOL_SERVICE_NAME)
+		if err != nil {
+			return app.Logger().PushFatalStack("failed to find service with specified name", err, logger.Fields{"name": s.config.POOL_SERVICE_NAME})
+		}
+
+		if service.TypeName() != s.config.POOL_SERVICE_TYPE {
+			return app.Logger().PushFatalStack("invalid service type", err, logger.Fields{"name": s.config.POOL_SERVICE_NAME, "service_type": s.config.POOL_SERVICE_TYPE, "pool_service_type": service.TypeName()})
+		}
+
+		if service.Provider() != app.Application() {
+			return app.Logger().PushFatalStack("invalid service type", err, logger.Fields{"name": s.config.POOL_SERVICE_NAME, "service_type": s.config.POOL_SERVICE_TYPE, "pool_service_type": service.TypeName()})
+		}
+
+		// load server configuration from service
+		restApiServer.SetConfigFromPoolService(service, s.config.PRIVATE_POOL_SERVICE)
+	}
+
 	return nil
 }
 
