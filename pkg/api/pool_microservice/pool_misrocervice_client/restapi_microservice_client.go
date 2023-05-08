c0fcc791134e2ac5ca5dd3fad23814ae42129b8f
evgeniums <evgeniums@dracosha.com>
2023-05-08 20:26:29 +0300

    Fix URL construction in microservice client

--- pkg/api/pool_microservice/pool_misrocervice_client/restapi_microservice_client.go
+++ pkg/api/pool_microservice/pool_misrocervice_client/restapi_microservice_client.go
@@ -3,9 +3,11 @@ package pool_misrocervice_client
 import (
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/evgeniums/go-backend-helpers/pkg/api/api_client/rest_api_client"
 	"github.com/evgeniums/go-backend-helpers/pkg/pool"
+	"github.com/evgeniums/go-backend-helpers/pkg/utils"
 )
 
 type RestApiPoolServiceClient struct {
@@ -16,8 +18,12 @@ func (r *RestApiPoolServiceClient) InitForPoolService(service *pool.PoolServiceB
 
 	url := service.PrivateUrl()
 	if url == "" {
-		if service.PrivateHost() == "" {
+		host := service.PrivateHost()
+		if host == "" {
 			return errors.New("unknown URL of the service")
+		}
+		if !strings.HasPrefix(host, "http") {
+			host = utils.ConcatStrings("http://", host)
 		}
 		if service.PrivatePort() == 0 {
 			return errors.New("unknown port of the service")
@@ -26,7 +32,7 @@ func (r *RestApiPoolServiceClient) InitForPoolService(service *pool.PoolServiceB
 		if service.ApiVersion() != "" {
 			apiVersion = fmt.Sprintf("/%s", service.ApiVersion())
 		}
-		url = fmt.Sprintf("%s:%d%s%s", service.PrivateHost(), service.PrivatePort(), service.PathPrefix(), apiVersion)
+		url = fmt.Sprintf("%s:%d%s%s", host, service.PrivatePort(), service.PathPrefix(), apiVersion)
 	}
 
 	r.Client = rest_api_client.New(rest_api_client.DefaultRestApiClient(url, clientAgent...))
