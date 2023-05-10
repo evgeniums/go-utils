f6ef13a32d44b5784c35c513850e0a89e31cd19c
evgeniums <evgeniums@dracosha.com>
2023-05-10 19:41:24 +0300

    Support URL redirection in http requests

--- pkg/http_request/send_raw_request.go
+++ pkg/http_request/send_raw_request.go
@@ -7,12 +7,17 @@ import (
 	"github.com/evgeniums/go-backend-helpers/pkg/op_context"
 )
 
-func SendRawRequest(ctx op_context.Context, request *http.Request) (*http.Response, error) {
+type RedirectHandler func(req *http.Request, via []*http.Request) error
+
+func SendRawRequest(ctx op_context.Context, request *http.Request, redirectHandler ...RedirectHandler) (*http.Response, error) {
 
 	c := ctx.TraceInMethod("http_request.Send", logger.Fields{"url": request.URL.Path, "method": request.Method})
 	defer ctx.TraceOutMethod()
 
 	client := &http.Client{}
+	if len(redirectHandler) != 0 {
+		client.CheckRedirect = redirectHandler[0]
+	}
 	response, err := client.Do(request)
 	if err != nil {
 		return nil, c.SetError(err)
