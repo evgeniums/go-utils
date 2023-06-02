3b69f99fa42924fa02d394332c769494c89b8315
evgeniums <evgeniums@dracosha.com>
2023-06-02 19:32:17 +0300

    Add substr utility

--- pkg/utils/concatenate.go
+++ pkg/utils/concatenate.go
@@ -23,3 +23,17 @@ func BuildString(sb *strings.Builder, parts ...string) {
 		sb.WriteString(part)
 	}
 }
+
+func Substr(input string, start int, length int) string {
+	asRunes := []rune(input)
+
+	if start >= len(asRunes) {
+		return ""
+	}
+
+	if start+length > len(asRunes) {
+		length = len(asRunes) - start
+	}
+
+	return string(asRunes[start : start+length])
+}
