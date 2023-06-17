56c72310b443810a126f7588c926e57b04609e96
evgeniums <evgeniums@dracosha.com>
2023-06-17 18:53:28 +0300

    Support time fields in maptostruct

--- pkg/utils/conversions.go
+++ pkg/utils/conversions.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"math"
+	"reflect"
 	"strconv"
 	"strings"
 	"time"
@@ -164,13 +165,36 @@ func FloatAlmostEqual[T Float](a, b T) bool {
 	return math.Abs(float64(a)-float64(b)) <= float64EqualityThreshold
 }
 
+func toTimeHookFunc() mapstructure.DecodeHookFunc {
+	return func(
+		f reflect.Type,
+		t reflect.Type,
+		data interface{}) (interface{}, error) {
+		if t != reflect.TypeOf(time.Time{}) {
+			return data, nil
+		}
+
+		switch f.Kind() {
+		case reflect.String:
+			return time.Parse(time.RFC3339, data.(string))
+		case reflect.Float64:
+			return time.Unix(0, int64(data.(float64))*int64(time.Millisecond)), nil
+		case reflect.Int64:
+			return time.Unix(0, data.(int64)*int64(time.Millisecond)), nil
+		default:
+			return data, nil
+		}
+		// Convert it by parsing
+	}
+}
+
 func MapToStruct(in interface{}, out interface{}, tag ...string) error {
 
 	t := OptionalArg("json", tag...)
 
 	// create new map decoder
 	meta := &mapstructure.Metadata{}
-	config := &mapstructure.DecoderConfig{Metadata: meta, TagName: t, Result: out, Squash: true, WeaklyTypedInput: true}
+	config := &mapstructure.DecoderConfig{Metadata: meta, TagName: t, Result: out, Squash: true, WeaklyTypedInput: true, DecodeHook: mapstructure.ComposeDecodeHookFunc(toTimeHookFunc())}
 	dec, err := mapstructure.NewDecoder(config)
 	if err != nil {
 		return err
