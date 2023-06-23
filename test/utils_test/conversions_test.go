dec51466866d9126014af1ae3093074ac84ed470
evgeniums <evgeniums@dracosha.com>
2023-06-23 21:02:37 +0300

    Add more tests for float-string conversions

--- test/utils_test/conversions_test.go
+++ test/utils_test/conversions_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/evgeniums/go-backend-helpers/pkg/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func CheckEqual(t *testing.T, value string, sample string) {
@@ -20,6 +21,7 @@ func TestFloatToStr(t *testing.T) {
 	f4 := float64(1000.111)
 	f5 := float64(1000.1111)
 	f6 := float64(1000.1175)
+	f7 := float64(1000.1)
 
 	// t.Logf(utils.FloatToStr2(f1))
 	// t.Logf(utils.FloatToStr2(f2))
@@ -36,4 +38,42 @@ func TestFloatToStr(t *testing.T) {
 	CheckEqual(t, utils.FloatToStr2(f4), "1000.11")
 	CheckEqual(t, utils.FloatToStr2(f5), "1000.11")
 	CheckEqual(t, utils.FloatToStr2(f6), "1000.12")
+	CheckEqual(t, utils.FloatToStr(f7), "1000.1")
+
+	ff1, e := utils.StrToFloat(utils.FloatToStr(f1))
+	assert.NoError(t, e)
+	assert.InDelta(t, f1, ff1, 0.0000001)
+
+	ff2, e := utils.StrToFloat(utils.FloatToStr(f2))
+	assert.NoError(t, e)
+	assert.InDelta(t, f2, ff2, 0.0000001)
+
+	ff3, e := utils.StrToFloat(utils.FloatToStr(f3))
+	assert.NoError(t, e)
+	assert.InDelta(t, f3, ff3, 0.0000001)
+
+	ff4, e := utils.StrToFloat(utils.FloatToStr(f4))
+	assert.NoError(t, e)
+	assert.InDelta(t, f4, ff4, 0.0000001)
+
+	ff5, e := utils.StrToFloat(utils.FloatToStr(f5))
+	assert.NoError(t, e)
+	assert.InDelta(t, f5, ff5, 0.0000001)
+
+	ff6, e := utils.StrToFloat(utils.FloatToStr(f6))
+	assert.NoError(t, e)
+	assert.InDelta(t, f6, ff6, 0.0000001)
+
+	ff7, e := utils.StrToFloat(utils.FloatToStr(f7))
+	assert.NoError(t, e)
+	assert.InDelta(t, f7, ff7, 0.0000001)
+}
+
+func TestStrToFloat(t *testing.T) {
+
+	str := "1000.1"
+	f1, e := utils.StrToFloat(str)
+	assert.NoError(t, e)
+
+	t.Logf("%s", utils.FloatToStr2(f1))
 }
