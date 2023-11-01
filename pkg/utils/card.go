cc05d0ef76c8f6ced22eefcdc50f844e7ff66a8c
evgeniums <evgeniums@dracosha.com>
2023-11-01 20:20:36 +0300

    Update card formatting helpers

--- pkg/utils/card.go
+++ pkg/utils/card.go
@@ -1,14 +1,27 @@
 package utils
 
-func MaskCard(pan string) string {
+func MaskCard(pan string, maskPrefix ...bool) string {
 	size := len(pan)
 	res := ""
+	noPrefix := OptionalArg(false, maskPrefix...)
 	for i := 0; i < size; i++ {
-		if i < 6 || i > (size-5) {
+		if (!noPrefix && i < 6) || i > (size-5) {
 			res += string(pan[i])
 		} else {
 			res += "*"
 		}
 	}
 	return res
+}
+
+func FormatCard(pan string) string {
+	result := ""
+	for i := 0; i < len(pan); i++ {
+		if i != 0 && i%4 == 0 {
+			result += " "
+		}
+		result += string(pan[i])
+	}
+
+	return result
 }
