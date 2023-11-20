faaa6b0f8de16d2667dbf228612d2a592c6e5772
evgeniums <evgeniums@dracosha.com>
2023-11-20 10:56:12 +0300

    Add rsa private key test

---
+++ test/crypt_test/rsa_test.go
@@ -0,0 +1,24 @@
+package crypt_test
+
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/evgeniums/go-backend-helpers/pkg/crypt_utils"
+	"github.com/evgeniums/go-backend-helpers/pkg/test_utils"
+	"github.com/stretchr/testify/assert"
+)
+
+var _, testBasePath, _, _ = runtime.Caller(0)
+var testDir = filepath.Dir(testBasePath)
+
+func TestLoadPrivateRsaKey(t *testing.T) {
+
+	keyPath := test_utils.AssetsFilePath(testDir, "private.key")
+	passphrase := "12345"
+
+	signer := crypt_utils.NewRsaSigner()
+	err := signer.LoadKeyFromFile(keyPath, passphrase)
+	assert.NoError(t, err)
+}
